d3bb435b0e125effa1ff25a5f58828085065e9f5
ethan-pt <tubbeethan@gmail.com>
2024-10-21 00:20:00 -0700

    add image display elements upon directory selection

--- renderer.js
+++ renderer.js
@@ -11,4 +11,21 @@ browseBtn.addEventListener('click', () => {
 // Event listener for the response from the main process
 ipcRenderer.on('selected-directory', (event, path) => {
     console.log('Selected directory: ', path);
+
+    const imgContainer = document.getElementById('imgContainer');
+    
+    // For now, I'm just going to have users select at single image at a time.
+    // Once I get handle on that, it will likely be changed to only directories.
+    // TODO: keep main.js congruent with ^^^
+    const imageElement = document.createElement('img');
+
+    const fs = require('fs');
+    const buf = fs.readFileSync(path);
+
+    const dcraw = require('dcraw');
+    const image = dcraw(buf, { extractThumbnail: True });
+
+    imageElement.src = image;
+
+    imgContainer.appendChild(imageElement);
 });
